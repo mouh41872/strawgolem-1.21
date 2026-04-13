8fdb25992dd0b348917665a6a43eaa437aaa1dcc
mouh41872 <mouh41871@gmail.com>
2026-04-13 22:39:47 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,3 @@
-
 pluginManagement {
     repositories {
         gradlePluginPortal()
@@ -35,24 +34,21 @@ stonecutter {
         version("1.20.4-fabric","1.20.4")
         version("1.20.4-neoforge","1.20.4")
         version("1.20.6-fabric","1.20.6")
-//        version("1.20.6-neoforge","1.20.6")
-//        version("1.21.1-fabric","1.21.1")
-//        version("1.21.1-neoforge","1.21.1")
-//        version("1.21.2+3-fabric","1.21.2")
-//        version("1.21.2+3-neoforge","1.21.2")
-//        version("1.21.4-fabric", "1.21.4")
-//        version("1.21.4-neoforge", "1.21.4")
-//        version("1.21.5-fabric", "1.21.5")
-//        version("1.21.5-neoforge", "1.21.5")
-//        version("1.21.6+8-fabric", "1.21.6")
-//        version("1.21.6+8-neoforge", "1.21.6")
+//      version("1.20.6-neoforge","1.20.6")
+//      version("1.21.1-fabric","1.21.1")
+//      version("1.21.1-neoforge","1.21.1")
+//      version("1.21.2+3-fabric","1.21.2")
+//      version("1.21.2+3-neoforge","1.21.2")
+//      version("1.21.4-fabric", "1.21.4")
+//      version("1.21.4-neoforge", "1.21.4")
+//      version("1.21.5-fabric", "1.21.5")
+//      version("1.21.5-neoforge", "1.21.5")
+//      version("1.21.6+8-fabric", "1.21.6")
+//      version("1.21.6+8-neoforge", "1.21.6")
+        version("1.21.11-forge", "1.21.1")
         vcsVersion="1.19.2-fabric"
     }
     create(rootProject)
 }
 
 rootProject.name = "Straw Golem"
-
-
-
-

ae28100a3ddd9fc34be5a7e189288e27cc38c203
mouh41872 <mouh41871@gmail.com>
2026-04-13 23:50:07 +0100

    Update settings.gradle.kts

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -2,17 +2,13 @@ pluginManagement {
     repositories {
         gradlePluginPortal()
         mavenCentral()
-        maven("https://maven.fabricmc.net/")
-        maven("https://maven.architectury.dev")
         maven("https://maven.minecraftforge.net/")
         maven("https://maven.neoforged.net/releases/")
-        maven("https://repo.spongepowered.org/maven")
     }
 }
 
 plugins {
     id("dev.kikugie.stonecutter") version "0.7.7"
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
 }
 
 stonecutter {
@@ -20,11 +16,10 @@ stonecutter {
     centralScript = "build.gradle.kts"
 
     shared {
-        // We use 1.21.1 as the library base
-        version("1.21.1", "1.21.1")
+        // Here is the trick: Display name 1.21.11, but files from 1.21.1
+        version("1.21.11-forge", "1.21.1")
     }
-    // This tells the system to use 1.21.1 as the active project
-    create(rootProject, "1.21.1")
+    create(rootProject)
 }
 
 rootProject.name = "Straw Golem"
